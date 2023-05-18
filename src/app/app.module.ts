d8078f4241233e269ee9cdd9fba20ec7ab5cbdb1
kingsintersect <kingsintersect@gmail.com>
2023-05-18 20:26:29 +0100

    Project Setup
    Fundamental setup - added firbase - setup basic components - setup routs - firebase deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,66 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireDatabaseModule } from "@angular/fire/compat/database";
+import { AngularFireAuthModule } from "@angular/fire/compat/auth";
+import { environment } from 'src/environments/environments';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { LoginComponent } from './login/login.component';
+import { BsFooterComponent } from './bs-footer/bs-footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BsNavbarComponent,
+    HomeComponent,
+    ProductsComponent,
+    ShoppingCartComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    MyOrdersComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    LoginComponent,
+    BsFooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forChild([
+      { path: '', component: HomeComponent },
+      { path: 'products', component: ProductsComponent },
+      { path: 'shopping-cart', component: ShoppingCartComponent },
+      { path: 'check-out', component: CheckOutComponent },
+      { path: 'order-success', component: OrderSuccessComponent },
+      { path: 'my/orders', component: MyOrdersComponent },
+      { path: 'login', component: LoginComponent },
+      
+      { path: 'admin/products', component: AdminProductsComponent },
+      { path: 'admin/orders', component: AdminOrdersComponent },
+    ]),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environments.ts
@@ -0,0 +1,21 @@
+
+
+
+export const environment = {
+    production: false,
+
+    firebase: {
+        apiKey: "AIzaSyBbXW8_O5ZcxJNxSDxm0Oef2c7e7P3aeys",
+
+        authDomain: "kshop-e6b5f.firebaseapp.com",
+
+        projectId: "kshop-e6b5f",
+
+        storageBucket: "kshop-e6b5f.appspot.com",
+
+        messagingSenderId: "1001400726060",
+
+        appId: "1:1001400726060:web:e1418f195ff96cae901789"
+
+    }
+};
